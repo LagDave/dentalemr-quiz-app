b7f4983cf60751c901185a62e2773409848778dc
lagdave <laggy80@gmail.com>
2024-05-15 02:24:56 +0800

    add google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import TopBar from "./sections/TopBar";
 import QuizContainer from "./sections/QuizContainer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Hero from "./sections/Hero";
+import ReactGA from "react-ga4";
+const TRACKING_ID = "G-VZJCZT58F3"; // your Measurement ID
 
 function App() {
   const [percentage, setPercentage] = useState(0);
@@ -10,6 +12,15 @@ function App() {
   function handleRefs(refs: (HTMLDivElement | null)[]) {
     setFirstItemRef(refs[0]);
   }
+
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+    ReactGA.send({
+      hitType: "pageview",
+      page: "/apps/assessment",
+      title: "DentalEMR Assessment",
+    });
+  }, []);
 
   return (
     <div className="bg-gray-50/30">
